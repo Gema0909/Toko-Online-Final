6885b7e2a361016ce60bef24e62106e807d66e5a
gema0909 <gemasyahdanprasetyo12@gmail.com>
2026-07-09 12:26:09 +0700

    UAS

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+MYSQL_DATABASE = "railway"
+MYSQL_PUBLIC_URL = "mysql://root:vomeikfGLsDLkddsaCmpvGNBxWgDzarC@hayabusa.proxy.rlwy.net:11147/railway"
+MYSQL_ROOT_PASSWORD = "vomeikfGLsDLkddsaCmpvGNBxWgDzarC"
+MYSQL_URL = "mysql://root:vomeikfGLsDLkddsaCmpvGNBxWgDzarC@mysql.railway.internal:3306/railway"
+MYSQLDATABASE = "railway"
+MYSQLHOST = "mysql.railway.internal"
+MYSQLPASSWORD = "vomeikfGLsDLkddsaCmpvGNBxWgDzarC"
+MYSQLPORT = "3306"
+MYSQLUSER = "root"
